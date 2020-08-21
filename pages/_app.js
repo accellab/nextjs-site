8aea97c40b0ae6536d74bf314c632c11af85c273
Billy Rizky J <billy@fitco.id>
2020-08-21 22:41:01 +0700

    #Initial project setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+// Components
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Alata&family=Lato:wght@300;400;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import Axios from 'axios';
+
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Accellab Digital Company</title>
+        <link rel='icon' href='/accellab-logo.png' />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className={styles.row}>
+        <h1 className={styles.title}>Accellab Digital Company.</h1>
+        <img
+          src='/accellab-logo.png'
+          alt='Accellab Logo'
+          className={styles.logo}
+        />
+      </div>
     </div>
-  )
+  );
 }
+
+// export async function getStaticProps() {
+//   const { data } = await Axios({
+//     url: `http://localhost:8080/api/blog/posts`,
+//     headers: {
+//       'x-custom-accelab-api-guest-authentication': 'ACCELLAB',
+//     },
+//     method: 'GET',
+//     params: '',
+//     body: '',
+//   });
+//   return {
+//     props: {
+//       posts: data.data,
+//     },
+//   };
+// }
